40a4adf0f1dcf36983c8bff3b491e646ef1af6ec
Dmitry Derepko <xepozz@list.ru>
2021-06-14 11:11:03 +0300

    Add ability to disable sentry

--- config/providers.php
+++ config/providers.php
@@ -4,6 +4,16 @@ declare(strict_types=1);
 
 use Yiisoft\Yii\Sentry\Provider\SentryProvider;
 
+/**
+ * @var array $params
+ */
+
+$enabled = (bool)($params['yiisoft/yii-sentry']['enabled'] ?? true);
+
+if (!$enabled) {
+    return [];
+}
+
 return [
     SentryProvider::class => SentryProvider::class,
 ];
